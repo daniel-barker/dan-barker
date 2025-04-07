04c0b4818d4ce6d464c33356eb1c61b3b98045b5
dan barker <dbarker16@gmail.com>
2025-04-07 14:15:22 -0400

    i missed a couple things

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import '../app.css';
+
+	injectAnalytics();
+	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
