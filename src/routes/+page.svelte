dd9edaf56543bb88d6457e606563e76a2a24e41f
dan barker <dbarker16@gmail.com>
2025-04-07 14:11:00 -0400

    center'd

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<img src="/images/dan-barker.jpg" alt="Dan Barker" />
+<div class="container">
+  <img src="/images/dan-barker.jpg" alt="Dan Barker" />
+</div>
+
+<style>
+  .container {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
